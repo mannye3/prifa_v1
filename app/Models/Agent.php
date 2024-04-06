ac547754a35a31373898e78ede6643de97cde154
iimmaannuueell <ebube.okeke@hotmail.com>
2024-04-06 12:30:00 +0100

    agent auth guard set

--- app/Models/Agent.php
+++ app/Models/Agent.php
@@ -5,10 +5,13 @@ namespace App\Models;
 use App\Models\Traits\HasUuid;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Agent extends Model
+class Agent extends Authenticatable
 {
     use HasFactory, HasUuid;
+
+    protected $guard = 'agent';
 
     protected $keyType = 'string';
     public $incrementing = false;
